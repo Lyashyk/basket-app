f35ac5beeb77bd00bd60a3a9384aca121c4920e3
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-03-16 13:22:21 +0200

    fix: buttom-icon-stories

--- src/components/button-icon/button-icon.stories.jsx
+++ src/components/button-icon/button-icon.stories.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withKnobs, select } from '@storybook/addon-knobs';
+
 import ButtonIcon from './button-icon';
+import IconTrash from '../../assets/icons/icon-trash/icon-trash';
 
 const sizes = {
   Normal: 'normal',
   Small: 'small'
 };
 
-// storiesOf('UI|Button', module).add('ButtonIcon', () => {
-//   return <ButtonIcon />;
-// });
-
 storiesOf('UI|Button', module)
   .addDecorator(withKnobs)
   .add('ButtonIcon', () => {
     const size = select('Size', sizes, 'normal');
 
-    return <ButtonIcon size={size} />;
+    return <ButtonIcon size={size} icon={<IconTrash />} />;
   });
