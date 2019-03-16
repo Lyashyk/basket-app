62edf7626a0e48805d0d7a1bf5acbee168648116
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-03-16 13:21:41 +0200

    fix: button-icon

--- src/components/button-icon/button-icon.jsx
+++ src/components/button-icon/button-icon.jsx
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 import cn from 'classnames';
 
 import './button-icon.css';
-import IconTrash from '../../assets/icon/icon-trash/icon-trash';
 
 class ButtonIcon extends Component {
   render() {
-    const { size } = this.props;
+    const { size, icon } = this.props;
 
     return (
       <button className={cn('ButtonIcon', `ButtonIcon--size-${size}`)}>
-        <div className="ButtonIcon-Wrapper">
-          <IconTrash />
-        </div>
+        <span className="ButtonIcon-Wrapper">{icon}</span>
       </button>
     );
   }
