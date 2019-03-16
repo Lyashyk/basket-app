539691eddfd3a21559ad29235a6f0edb55942883
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-03-16 14:09:40 +0200

    fix: catalog-product-card

---
+++ src/components/product-store-card/catalog-product-card.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import Button from '../button/button';
+import './catalog-product-card.css';
+
+class CatalogProductCard extends Component {
+  render() {
+    return (
+      <div className="CatalogProductCard">
+        <div className="Product-closeButtton">
+          <div className="Stub" />
+        </div>
+        <div className="CatalogProductCard-grid">
+          <div className="CatalogProductCard-imageColumn">
+            <img
+              className="CatalogProductCard-image"
+              src="https://cs8.pikabu.ru/post_img/2016/01/28/8/14539843261780265.jpg"
+              alt=""
+            />
+          </div>
+          <div className="CatalogProductCard-contentColumn">
+            <div className="CatalogProductCard-textContent">
+              <h2 className="CatalogProductCard-title">Картофель</h2>
+              <div className="CatalogProductCard-amount">Количество: 10</div>
+              <div className="CatalogProductCard-price">Цена: 1</div>
+            </div>
+            <div className="CatalogProductCard-buttonContent">
+              <div className="CatalogProductCard-button">
+                <Button title="Добавить в корзину" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CatalogProductCard;
