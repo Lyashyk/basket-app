26607b13a4c35a37b00768ef6588bfb84538d523
Aleksndr Illyashenko <alex1002x@gmail.com>
2019-03-16 14:43:33 +0200

    fix: catalog-product-card-storybook

--- src/components/product-store-card/catalog-product-card.stories.jsx
+++ src/components/product-store-card/catalog-product-card.stories.jsx
@@ -4,7 +4,7 @@ import { storiesOf } from '@storybook/react';
 
 import ProductStoreCard from './catalog-product-card';
 
-storiesOf('Modules|Product|CatalogProductCard', module).add('Default', () => {
+storiesOf('Modules|Product/CatalogProductCard', module).add('Default', () => {
   return (
     <div style={{ width: '600px' }}>
       <ProductStoreCard />
